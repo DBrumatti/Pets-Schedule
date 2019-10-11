8730a09adef0e0a5337f4ad1f3f3630393371f52
EnivaldoPinheiro <56415884+EnivaldoPinheiro@users.noreply.github.com>
2019-10-11 20:16:56 -0300

    inicio

---
+++ Animal.cs
@@ -0,0 +1,20 @@
+class Animal{
+  private string nome;
+  private string raca;
+  private string tipo;
+  private string cor;
+  private double peso;
+
+  public Animal(){}
+
+  public Animal(string nome,string raca,string tipo,string cor,double peso){
+
+    this.nome = nome;
+    this.raca = raca;
+    this.tipo = tipo;
+    this. cor = cor;
+    this.peso = peso;
+
+  }
+  public void comer(double Kg)
+}
---
+++ Dono.cs
@@ -0,0 +1,35 @@
+class Dono{
+  private string nome;
+  private int rg;
+  private string end;
+  private string cidade;
+
+  public Dono(string nome,int rg,string end,string cidade){
+
+  }
+  public void setNome(string nome){
+    this.nome=nome;
+  }
+  public getNome(){
+    return this.nome;
+  }
+  public void setRg(int rg ){
+    this.nome=nome;
+  }
+  public getRg(){
+    return this.rg;
+  }
+  public void setEnd(string end){
+    this.end=end;
+  }
+  public getEnd(){
+    return this.end;
+  }
+  public void setCidade(string cidade){
+    this.cidade=cidade;
+  }
+  public getCidade(){
+    return this.cidade;
+  }
+  
+}
